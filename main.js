4e683d69a83663aba12529d055281f86ecb4d214
Denys <d.o.sheremet@student.tudelft.nl>
2022-02-09 20:37:48 +0100

    can only type from beginning

--- main.js
+++ main.js
@@ -10,13 +10,13 @@ window.addEventListener("load", function () {
     const $search = document.getElementById('search');
     var prevState = "";
 
-
     let quote = quotes[randomBetween(0, quotes.length - 1)];
     $box.innerHTML = quote;
 
+
     $search.addEventListener('input', (event) => {
         var searchText = event.target.value;
-        const regex = new RegExp(searchText, 'gi');
+        const regex = new RegExp(searchText, 'i');
 
         let text = $box.innerHTML;
         let prevText = text;
@@ -26,8 +26,9 @@ window.addEventListener("load", function () {
         $box.innerHTML = newText;
 
         let newCounter = document.getElementsByClassName("highlight")[0].innerHTML.length;
-        if (newCounter > counter) {
+        if (newCounter > counter && $box.innerHTML[0] === "<") {
             counter = newCounter;
+            $search.value = document.getElementsByClassName("highlight")[0].innerHTML;
         } else {
             $search.value = prevState;
             $box.innerHTML = prevText;
