51e074b0dfa0630a6e445dcbc7e35037dcfd3aa6
Denys <d.o.sheremet@student.tudelft.nl>
2022-02-09 20:21:15 +0100

    mistake-tolerant

--- main.js
+++ main.js
@@ -1,26 +1,38 @@
 const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
+var counter = 0;
+
+
 
 
 window.addEventListener("load", function () {
 
     const $box = document.getElementById('box');
     const $search = document.getElementById('search');
+    var prevState = "";
 
 
-    let quote = quotes[randomBetween(0, quotes.length-1)];
+    let quote = quotes[randomBetween(0, quotes.length - 1)];
     $box.innerHTML = quote;
 
-
-
     $search.addEventListener('input', (event) => {
-        const searchText = event.target.value;
+        var searchText = event.target.value;
         const regex = new RegExp(searchText, 'gi');
 
         let text = $box.innerHTML;
+        let prevText = text;
         text = text.replace(/(<mark class="highlight">|<\/mark>)/gim, '');
 
-        const newText = text.replace(regex, '<mark class="highlight">$&</mark>');
+        var newText = text.replace(regex, '<mark class="highlight">$&</mark>');
         $box.innerHTML = newText;
+
+        let newCounter = document.getElementsByClassName("highlight")[0].innerHTML.length;
+        if (newCounter > counter) {
+            counter = newCounter;
+        } else {
+            $search.value = prevState;
+            $box.innerHTML = prevText;
+        }
+        prevState = $search.value;
     });
 
 });
